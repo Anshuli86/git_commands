742c89f0e87d199c2c2182dd8c82df6726134932
Anshuli86 <anshulipatilsp@gmail.com>
2025-07-18 14:42:19 +0530

    Update Commands.java

--- Commands.java
+++ Commands.java
@@ -1,7 +1,13 @@
 public class Commands{
 	
 	public void gitInit() {
-        System.out.println("git init - Initializes a new Git 	repository.");
-    }
+        System.out.println("git init - Initializes a new Git repository.");
+    	}
+	public void gitStatus() {
+        System.out.println("git status - Shows the working directory status.");
+    	}
+    	public void gitAdd() {
+        System.out.println("git add . - Stages all changes for commit.");
+    	}
 
 }
